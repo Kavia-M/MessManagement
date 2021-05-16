d2e21a3e25c86c02617b040618c53028ef8f4777
unknown <kaviaiit@gmail.com>
2021-05-16 15:02:42 +0530

    0th commit

---
+++ Mess-Management-App/src/test/java/com/messmanagementapp/Mess/Management/App/MessManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messmanagementapp.Mess.Management.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
